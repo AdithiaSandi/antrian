775fe284510a6a51982cf65dd11bf411a3f7edbf
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-03-14 14:35:20 +0700

    test fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,39 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const obj = {
+  nama: "test",
+  poli: "asd",
+  dokter: "lmao",
+  handphone: "0123456789",
+};
+
+// ADD DUMMY INTO LOCALSTORAGE
+const checkData = JSON.parse(localStorage.getItem("antrian"));
+let arr = [];
+if (checkData == undefined) {
+  for (let i = 1; i <= 80; i++) {
+    let lot = Math.floor(Math.random() * 2);
+    if (lot == 0) {
+      lot = Math.floor(Math.random() * 4);
+      if (lot == 0) {
+        arr.push({ ...obj, terpilih: true, batal: false, antrian: i });
+      } else if (lot == 1) {
+        arr.push({ ...obj, terpilih: false, batal: true, antrian: i });
+      } else if (lot == 2) {
+        arr.push({ ...obj, terpilih: true, batal: true, antrian: i });
+      } else {
+        arr.push({ ...obj, antrian: i });
+      }
+    } else {
+      arr.push({ antrian: i });
+    }
+  }
+
+  localStorage.setItem("antrian", JSON.stringify(arr));
+}
+
 root.render(
   <React.StrictMode>
     <App />
